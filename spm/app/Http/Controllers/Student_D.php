2f2a559cd9479708c7d74dbdbd9f2cf6d00eb550
shovonploan <shovonploan@gmail.com>
2021-05-08 18:28:53 +0600

    ui-s-f-a

--- spm/app/Http/Controllers/Student_D.php
+++ spm/app/Http/Controllers/Student_D.php
@@ -4,20 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class Student_D extends Controller
 {
 
     public function index($id)
     {
-        $user=User::where('user_id',$id)->get();
-        return view('pages/student/studentdashboard')->with(['users'=>$user]);
+        $user=User::where('user_id',$id)->first();
+        return redirect('/student')->with(['users'=>$user]);
+        // return view('pages/student/studentdashboard')->with(['user'=>$user]);
     }
     public function oR($id){
-        $user=User::where('user_id',$id)->get();
+        $user=User::where('user_id',$id)->first();
         // return redirect('/student',['users'=>$user]);
-        return redirect('/student')->with(['users'=>$user]);
-        return view('pages/student/overallReport')->with(['users'=>$user]);
+        return redirect('/overallReport')->with(['users'=>$user]);
+    }
+    public function showOR(){
+        $user=Session::get('users');
+        return view('pages/student/overallReport')->with(['user'=>$user]);
+    }
+    public function showH(){
+        $user=Session::get('users');
+        return view('pages/student/studentdashboard')->with(['user'=>$user]);
     }
 
 }
